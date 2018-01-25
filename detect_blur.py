c1431d0717cbb47d776c35b0b8f1e687690af0e4
adcrn <9167686+adcrn@users.noreply.github.com>
2018-01-25 15:58:01 -0500

    Abstracted necessary functions

--- detect_blur.py
+++ detect_blur.py
@@ -1,38 +1,35 @@
-# import the necessary packages
-from imutils import paths
-import argparse
-import cv2
- 
-def variance_of_laplacian(image):
-	# compute the Laplacian of the image and then return the focus
-	# measure, which is simply the variance of the Laplacian
-	return cv2.Laplacian(image, cv2.CV_64F).var()
- 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--images", required=True,
-	help="path to input directory of images")
-ap.add_argument("-t", "--threshold", type=float, default=100.0,
-	help="focus measures that fall below this value will be considered 'blurry'")
-args = vars(ap.parse_args())
+# UCF Senior Design 2017-18
+# Group 38
 
-# loop over the input images
-for imagePath in paths.list_images(args["images"]):
-	# load the image, convert it to grayscale, and compute the
-	# focus measure of the image using the Variance of Laplacian
-	# method
-	image = cv2.imread(imagePath)
-	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	fm = variance_of_laplacian(gray)
-	text = "Not Blurry"
- 
-	# if the focus measure is less than the supplied threshold,
-	# then the image should be considered "blurry"
-	if fm < args["threshold"]:
-		text = "Blurry"
- 
-	# show the image
-	cv2.putText(image, "{}: {:.2f}".format(text, fm), (10, 30),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
-	cv2.imshow("Image", image)
-	key = cv2.waitKey(0)
+import cv2
+
+THRESHOLD = 100.0
+
+
+def variance(image_path):
+    """
+        Calculate the variance of Laplacian distribution, which is a
+        measure of the sharpness of an image. Blurry pictures have less
+        regions containing rapid intensity changes, meaning that there will
+        be less of a spread of responses (low variance). Sharper images will
+        have a higher spread of responses (high variance).
+            image_path:     (String) path to the image being tested
+    """
+    image = cv2.imread(image_path)
+    greyscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    return cv2.Laplacian(greyscale, cv2.CV_64F).var()
+
+
+def check_sharpness(image, threshold=THRESHOLD):
+    """
+        Determine whether the sharpness of an image exceeds the threshold
+        for variance. Those surpassing the threshold will return True.
+            image:          (String) path to the image being tested
+            threshhold      (Float)  minimum variance for acceptance
+    """
+    sharpness = variance(image)
+
+    if sharpness > threshold:
+        return True
+    else:
+        return False
