74b3cfa4787dab2b6afa170bfc326a90c7a41f5d
adcrn <9167686+adcrn@users.noreply.github.com>
2018-01-26 20:13:41 -0500

    model_train.py now requires that a name be given for model weight output

--- model_train.py
+++ model_train.py
@@ -2,13 +2,25 @@
 # Group 38
 
 from model import Model
-import tflearn
-import pickle
+import argparse
 import json
 import os
+import pickle
+import tflearn
+
+# Create the models folder, if it doesn't exist already
+MODEL_SAVE_FOLDER = "models"
+os.makedirs(MODEL_SAVE_FOLDER, exist_ok=True)
+
+# Read in choice for model name (without the .tfl extension)
+parser = argparse.ArgumentParser()
+parser.add_argument("model_name", help="filename for trained model output")
+args = parser.parse_args()
 
 # Your chosen hyperparamters should be contained in a JSON
 HYPERPARAMETERS = "config/ad_hp.json"
+
+CHECKPOINT_NAME = args.model_name + ".ckpt"
 
 # Decode into a JSON object
 with open(HYPERPARAMETERS) as json_file:
@@ -19,7 +31,7 @@ test = Model('CNNTest', hp_content=hp_content)
 
 # Build the network through TFLearn
 model = tflearn.DNN(test.network, tensorboard_verbose=3,
-                    checkpoint_path=os.path.join(test.checkpoint_dir, 'test.ckpt'))
+                    checkpoint_path=os.path.join(test.checkpoint_dir, CHECKPOINT_NAME))
 
 # Open up the pickled dataset (although this may change with TFRecord stuff)
 with open('full_dataset.pkl', 'rb') as f:
@@ -34,5 +46,7 @@ model.fit(X, Y, n_epoch=test.hp.num_epochs, shuffle=True,
           show_metric=True, batch_size=test.hp.batch_size,
           run_id='bird-classifier')
 
-# Save the model, update # every new run to store all of our results
-model.save("saved/train1/bird-classifier1.tfl")
+# Save the model under the name given at start
+model_name = args.model_name + ".tfl"
+model_path = os.path.join(MODEL_SAVE_FOLDER, model_name)
+model.save(model_path)
