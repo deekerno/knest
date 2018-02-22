e4e5b2242fd45831e0478e2e227f966f7fac550d
Joe <joeleav1@gmail.com>
2018-02-22 15:39:32 -0500

    working :)

--- tf_object_detection/xml_to_csv.py
+++ tf_object_detection/xml_to_csv.py
@@ -25,7 +25,7 @@ def xml_to_csv(path):
 
 def main():
     for directory in ['train', 'test']:
-        image_path = os.path.join(os.getcwd(), 'bird_images/{}'.format(directory))
+        image_path = os.path.join(os.getcwd(), 'images/{}'.format(directory))
         xml_df = xml_to_csv(image_path)
         xml_df.to_csv('data/{}_labels.csv'.format(directory), index=None)
         print('Successfully converted xml to csv.')
