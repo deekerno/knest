c6291eb4728f0fdb38f15abbd4bf4617f3ccb5ce
Joe <joeleav1@gmail.com>
2018-02-22 15:39:32 -0500

    working :)

--- tf_object_detection/organize_model.sh
+++ tf_object_detection/organize_model.sh
@@ -1,5 +1,5 @@
-mv data/ models/research/object_detection
-mv bird_images/ models/research/object_detection
+mv bird_data/ models/research/object_detection
+mv images/ models/research/object_detection
 mv faster_rcnn_nas_coco_2017_11_08/ models/research/object_detection
 mv faster_rcnn_nas_coco.config training/
 mv training/ models/research/object_detection
