ee6385469b688a105afecffbc658e5c703b58133
JoeLeavitt <joeleav1@gmail.com>
2018-03-06 01:26:50 -0500

    add modified tf_api - training and inference working for faster_rcnn_nas

--- tf_object_detection/organize_model.sh
+++ tf_object_detection/organize_model.sh
@@ -1,5 +1,6 @@
 mv bird_data/ models/research/object_detection
 mv images/ models/research/object_detection
-mv faster_rcnn_nas_coco_2017_11_08/ models/research/object_detection
+mv faster_rcnn_nas_coco_*/ models/research/object_detection
 mv faster_rcnn_nas_coco.config training/
 mv training/ models/research/object_detection
+mv bird_inference.py models/research/object_detection
