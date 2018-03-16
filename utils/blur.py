29c5d74bc22ceeaeac5d06ced6c1d3c21df9ff6b
Nhi Nguyen <nhiquynhnguyen@yahoo.com>
2018-03-16 02:07:55 -0400

    Connected image comparison to GUI, which can be toggled on/off. Accepted images are now saved into a subdirectory called, 'processed'. Updated utils folder

--- utils/blur.py
+++ utils/blur.py
@@ -1,89 +1,170 @@
-# UCF Senior Design 2017-18
-# Group 38
-
 import cv2
-import scipy.fftpack as fp
 import numpy as np
 import pywt
 
-LAP_THRESHOLD = 100.0
+FIXED_SIZE = 1024
+EDGE_THRESH = 35
+MIN_ZERO = 0
+
+# partition edge maps into windows
+def partition(emap, rows, cols):
+    h, w = emap.shape
+
+    return (emap.reshape(h // rows, rows, -1, cols)
+            .swapaxes(1, 2)
+            .reshape(-1, rows, cols))
 
 
-def weighted_blur(image):
-    """
-        Calculate a weighted sum of certain blur detection metrics.
-            image:          (Array) Array representation of image
-    """
-    t_weight = 0.10 * teng(image)
-    f_weight = 0.10 * fft(image)
-    v_weight = 0.06 * lapm(image)
-    # sw_weight = 1e-7 * sum_wave(image)
+# haar wavelet transform calculation
+def haar_wavelet_transform(img):
+    coeffs = pywt.dwt2(img, 'haar')
 
-    return t_weight + f_weight + v_weight #+ sw_weight
+    # from pywt documentation: cA, (cH, cV, cD)
+    # cA: LL
+    # cH: LH
+    # cV: HL
+    # cD: HH
+    LL, (LH, HL, HH) = coeffs
 
-
-def variance(image):
-    """
-        Calculate the variance of Laplacian distribution, which is a
-        measure of the sharpness of an image. Blurry pictures have less
-        regions containing rapid intensity changes, meaning that there will
-        be less of a spread of responses (low variance). Sharper images will
-        have a higher spread of responses (high variance).
-            image:     (String) path to the image being tested
-    """
-    image = cv2.imread(image)
-    return image, cv2.Laplacian(image, cv2.CV_64F).var()
+    return LL, LH, HL, HH
 
 
-def teng(image):
-    """
-        Calculate the Tenegrad variance.
-            image:     (Array) Array (color/greyscale) representation of image
-    """
-    gauss_x = cv2.Sobel(image, cv2.CV_64F, 1, 0)
-    gauss_y = cv2.Sobel(image, cv2.CV_64F, 1, 0)
-    return np.mean((gauss_x * gauss_x) + (gauss_y * gauss_y))
+# calc emap, iterating over LL
+def calc_emap(LH, HL, HH):
+    # square root of LH^2 + HL^2 + HH^2
+    return np.sqrt((np.power(LH, 2) + np.power(HL, 2) + np.power(HH, 2)))
+    
+
+# find local maxima of each edgemap partition
+def calc_emax(emap, window_size):
+    # split edge map into windows sized (window_size X window_size)
+    # find the local maxima of each window
+    result = partition(emap, window_size, window_size)
+    max_points = []
+
+    # from 0 to the total number of partitions
+    for i in range(0, len(result)):
+        # find the max of each partition and
+        # add to total list of local maxes
+        max_points.append(np.max(result[i]))
+
+    # return list of all local maxes
+    return max_points
 
 
-def fft(image):
-    """
-        Calculate the mean of the fast fourier transform.
-            image:     (Array) Array (color/greyscale) representation of image
-    """
-    fft = fp.rfft(fp.rfft(image, axis=0), axis=1)
-    result = np.mean(fft)
-    return result
+def calc_values(emax1, emax2, emax3):
+    # total number of edge points
+    Nedge = 0
+    # total number of Dirac- or Astep-Structure edge points
+    Nda = 0
+    # total number of Roof- or Gstep-Structure edge points
+    Nrg = 0
+    # total number of Roof- or Gstep-Structure edge points
+    # that have lost their sharpness
+    Nbrg = 0
+
+    # all emax have the same length
+    for i in range(0, len(emax1)):
+        # Rule 1
+        if emax1[i] > EDGE_THRESH or emax2[i] > EDGE_THRESH or emax3[i] > EDGE_THRESH:
+            Nedge += 1
+
+            # Rule 2
+            if emax1[i] > emax2[i] and emax2[i] > emax3[i]:
+                Nda += 1
+
+            # Rule 3 & 4
+            elif emax1[i] < emax2[i]:
+                # Roof- or
+                if emax2[i] < emax3[i]:
+                    Nrg += 1
+
+                # Roof structure
+                elif emax2[i] > emax3[i]:
+                    Nrg += 1
+
+                # Rule 5
+                if emax1[i] < EDGE_THRESH:
+                    Nbrg += 1
+
+    return Nedge, Nda, Nrg, Nbrg
 
 
-def lapm(image):
-    """
-        Calculate the modified Laplacian variance.
-            image:     (Array) Array (color/greyscale) representation of image
-    """
-    kernel = np.array([-1, 2, 1])
-    lap_x = np.abs(cv2.filter2D(image, -1, kernel))
-    lap_y = np.abs(cv2.filter2D(image, -1, kernel.T))
-    return np.mean(lap_x + lap_y)
+def calc_intensities(img):
+    # i = 1
+    LL_1, LH_1, HL_1, HH_1 = haar_wavelet_transform(img)
+    emap1 = calc_emap(LH_1, HL_1, HH_1)
+    emax1 = calc_emax(emap1, 8)
+
+    # i = 2
+    LL_2, LH_2, HL_2, HH_2 = haar_wavelet_transform(LL_1)
+    emap2 = calc_emap(LH_2, HL_2, HH_2)
+    emax2 = calc_emax(emap2, 4)
+
+    # i = 3
+    LL_3, LH_3, HL_3, HH_3 = haar_wavelet_transform(LL_2)
+    emap3 = calc_emap(LH_3, HL_3, HH_3)
+    emax3 = calc_emax(emap3, 2)
+
+    return emax1, emax2, emax3
 
 
-def sum_wave(image):
-    """
-        Calculate the sum of coefficients given by the discrete wavelet transform.
-            image:     (Array) Array (color/greyscale) representation of image
-    """
-    coeffs = pywt.dwt2(image, 'db1')
-    cA, (cH, cV, cD) = coeffs
-    total = np.sum(cA) + np.sum(cH) + np.sum(cV) + np.sum(cD)
-    return total
+# default resize value is 1024; however, if image dimensions are smaller,
+# resize accordingly. Smallest resize will be 256 or FIXED_SIZE divided by 4
+def determine_resize(img):
+    size = FIXED_SIZE
+
+    if len(img[0]) < size:
+        if len(img[0]) < size // 2:
+            return size // 4
+        else:
+            return size // 2
+
+    return size
 
 
-def check_sharpness(image_path, threshold=LAP_THRESHOLD):
-    """
-        Determine whether the sharpness of an image exceeds the threshold
-        for variance. Those surpassing the threshold will return True.
-            image:          (String) path to the image being tested
-            threshhold      (Float)  minimum variance for acceptance
-    """
-    image, sharpness = variance(image_path)
+# classify whether image is blurry or not blurry
+# blur metrics vary based on resize value
+def blur_result(size, per, blur_extent):
+    if size == FIXED_SIZE:
+        if (per <= MIN_ZERO and blur_extent > .83) or blur_extent >= .9:
+            # blurry
+            return 0
+        else:
+            # not blurry
+            return 1
+    else:
+        if (per <= MIN_ZERO and blur_extent > .75) or blur_extent >= .9:
+            # blurry
+            return 0
+        else:
+            # not blurry
+            return 1
 
-    return image, sharpness, sharpness > threshold
+    # preventive measure
+    # default to not blurry result
+    return 1
+
+
+def detect_blur(img_path):
+    image = cv2.imread(img_path)
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    size = determine_resize(img)
+    img = cv2.resize(img, (size, size))
+
+    emax1, emax2, emax3 = calc_intensities(img)
+
+    # calculate Nedge, Nda, Nrg and Nbrg
+    Nedge, Nda, Nrg, Nbrg = calc_values(emax1, emax2, emax3)
+
+    # ratio of Dirac- and Astep-Structure to all edges
+    # if divisor is 0, set per to 0
+    per = Nedge == 0 and 0 or Nda / Nedge
+    # blur confident coefficient; how many Roof- and Gstep-Structure edges are
+    # blurred; if divisor is 0, set blur_extent to 0
+    blur_extent = Nrg == 0 and 0 or Nbrg / Nrg
+
+    result = blur_result(size, per, blur_extent)
+
+    return image, result
