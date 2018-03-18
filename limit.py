88dd74970c980c041e7d3ac3f1af84b2cf377707
Nhi Nguyen <nhiquynhnguyen@yahoo.com>
2018-03-18 12:40:12 -0400

    Added working progress bar for comparing images and writing to subdirectory. Condensed limit.py to take in a single image rather than a dictionary

--- limit.py
+++ limit.py
@@ -1,49 +1,27 @@
-from PIL import Image
 import utils.compare as compare
-import os
 
 DIFF_THRES = 20
 LIMIT = 2
 
 
-def limit(images, des_path):
-    # initialized variable
-    std = ''
-    count = 0
-    files = images.keys()
+def limit(img, std_hash, count):
+    # calculate hash for given image
+    cmp_hash = compare.calc_hash(img)
 
-    # go through dictionary of images
-    for filename in files:
-        if std == '':
-            std, std_hash, count = set_standard(images, filename, des_path)
+    # compare to standard
+    diff = compare.compare(std_hash, cmp_hash)
 
-        else:
-            # calculate hash for working image
-            cmp_hash = compare.calc_hash(images[filename])
-            # compare to standard
-            diff = compare.compare(std_hash, cmp_hash)
+    # if image is similar to standard
+    if diff <= DIFF_THRES:
+        # if there are 3 similar images already, return False
+        if count >= LIMIT:
+            return 'remove'
 
-            # image is similar to standard
-            if diff <= DIFF_THRES:
-                # if there are 3 similar images already, remove the rest from
-                # the image dictionary
-                if count >= LIMIT:
-                    images.pop(filename)
+    else:
+        return 'update_std'
 
-                # keep track of number of similar images
-                count += 1
-
-            # a non-similar image has been found, make it the new standard
-            # for comparison
-            else:
-                std, std_hash, count = set_standard(images, filename, des_path)
-
-    return images
+    return 'continue'
 
 
-def set_standard(images, filename, des_path):
-    # automatically save standard to processed folder
-    # img = Image.fromarray(images[filename])
-    # img.save(os.path.join(des_path, filename))
-
+def set_standard(images, filename):
     return filename, compare.calc_hash(images[filename]), 0
