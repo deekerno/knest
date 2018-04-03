448c67723034116c6b33b6402e83589d2133ddae
adcrn <9167686+adcrn@users.noreply.github.com>
2018-04-03 17:06:05 -0400

    Removed tf_object_detection folder, still resides in other branches

--- tf_object_detection/organize_model.sh
+++
@@ -1,6 +0,0 @@
-mv bird_data/ models/research/object_detection
-mv images/ models/research/object_detection
-mv faster_rcnn_nas_coco_*/ models/research/object_detection
-mv faster_rcnn_nas_coco.config training/
-mv training/ models/research/object_detection
-mv bird_inference.py models/research/object_detection
